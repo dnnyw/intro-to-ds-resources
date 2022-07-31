cf5221aba114f3a0eaa56e2eb1749dd2fbb07c2e
dnnyw <107650427+dnnyw@users.noreply.github.com>
2022-07-30 23:50:47 -0700

    update solutions

--- final_project/project_helper.py
+++ final_project/project_helper.py
@@ -1,5 +1,7 @@
+from csv import excel_tab
 import pandas as pd
 import numpy as np
+from sqlalchemy import except_all
 from functions import *
 
 from scipy import optimize
@@ -29,7 +31,7 @@ def minimize(f, start=None, smooth=False, log=None, array=False, **vargs):
         assert not array, "Please pass starting values explicitly when array=True"
         arg_count = f.__code__.co_argcount
         assert arg_count > 0, "Please pass starting values explicitly for variadic functions"
-        start = [10] * arg_count
+        start = [4] * arg_count
     if not hasattr(start, '__len__'):
         start = [start]
 
@@ -152,9 +154,166 @@ def standardize(an_array):
     return (an_array - np.mean(an_array))/np.std(an_array)
 
 
+# <----- AUTOGRADER ------>
+
+b = pd.read_csv('data/businesses.csv')
+i = pd.read_csv('data/inspections.csv')
+i_n = i.merge(right = b, how = 'inner', on = 'bid')
+s = i_n[i_n['score'] >0]
+
+# q4d1
+temp = s['bid'].value_counts()
+un = temp[temp >= 3].index
+
+# q4d2
+swi = np.array([])
+for bid in un:
+    subset = s[s['bid'] == bid]
+    diff = max(subset['score']) - min(subset['score'])
+    swi = np.append(swi, diff)
+
+#q5 onwards
+
+yr = s['rating']
+rc = s['review_count']
+p = s['price']
+lrc = np.log(rc)
 
 
+# q5c
+
+def msey(s, i):
+    error = yr - (s * lrc + i)
+    return np.mean(error**2)
 
 
+# q5d
+
+bs = np.corrcoef(yr, lrc)[0][1]*np.std(yr)/np.std(lrc)
+bi = np.mean(yr) - bs*np.mean(lrc)
+
+def plot_q5d(slope, intercept):
+    fig, ax = plt.subplots()
+    ax.scatter(lrd, yr)
+    lims = np.array([5, 9])
+    ax.plot(lims, slope*lims + intercept)
+    
+#q5e
+
+prices = ["$", "$$", "$$$", "$$$$"]
+slopes = []
+intercepts = []
+for price in prices:
+    data = s[s['price'] == price]
+    rating = data['rating']
+    review_num = np.log(data['review_count'])
+    slope = np.corrcoef(rating, review_num)[1,0]*np.std(rating)/np.std(review_num)
+    intercept = np.mean(rating) - slope*np.mean(review_num)
+    slopes.append(slope)
+    intercepts.append(intercept)
+slopes = np.array(slopes)
+intercepts = np.array(intercepts)
+
+results = {'q1a':False,
+    'q2a':False,
+    'q2b':False,
+    'q2c':False,
+    'q3a':False,
+    'q3b':False,
+    'q3c1':False,
+    'q4a':False,
+    'q4c':False,
+    'q4d1':False,
+    'q4d2':False,
+    'q4d3':False,
+    'q5b':False,
+    'q5c':False,
+    'q5d':False,
+    'q5e':False}
 
 
+def check(q, a):
+    if q == 'q1a':
+        try:
+            results[q] = (b.equals(a[0])) and (i.equals(a[1]))
+        except:
+            print('Something went wrong, try again!')
+    elif q == 'q2a':
+        try:
+            bus = a[0]
+            bid = a[1]
+            results[q] = (bus == 607) and (bid == 658)
+        except:
+            print('Answer in wrong format!')
+    elif q == 'q2b':
+        try:
+            results[q] = a == 'One Market Restaurant'
+        except:
+            print('Something went wrong, try again!')
+    elif q == 'q2c':
+        try:
+            results[q] = a == 29
+        except:
+            print('Something went wrong, try again!')
+    elif q == 'q3a':
+        try:
+            results[q] = a == b[b['name'] == 'One Market Restaurant']
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q3b':
+        try:
+            results[q] = all(a == b[(b['rating'] <= 4) & (b['review_count'] > 1000)]['price'].value_counts())
+        except:
+            print("Something went wrong, try again!")
+    elif q == "q3c1":
+        try:
+            temp = b[(b['latitude'] != -9999) & (b['longitude'] != -9999)]
+            results[q] = temp.equals(a)
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q4a':
+        try:
+            results[q] = s.equals(a)
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q4c':
+        try:
+            results[q] = a == 'Mr Szechuan'
+        except:
+            print("Answer in wrong format!")
+    elif q == 'q4d1':
+        try:
+            results[q] = all(a == un) or (len(a) == 334)
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q4d2':
+        try:
+            results[q] = all(a == swi)
+        except:
+            print("Something went wrong, try again!")  
+    elif q == 'q4d3':
+        try:
+            results[q] = a == 'Mr Szechuan'
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q5b':
+        try:
+            results[q] = all(a == lrc)
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q5c':
+        try:
+            results[q] = np.abs(msey(10,1) - a(10,1)) < 0.01
+        except:
+            print("Something went wrong, try again!")       
+    elif q == 'q5d':
+        try:
+            results[q] =  (np.abs(a[0] - bs) < 0.001) and (np.abs(a[1] - bi) < 0.1)
+            return [np.abs(a[0] - bs) < 0.001, np.abs(a[1] - bi) < 0.1]
+        except:
+            print("Something went wrong, try again!")
+    elif q == 'q5e':
+        try:
+            results[q] = sum(np.abs(slopes - a[0])) < 1 and sum(np.abs(intercepts - a[1])) < 1
+        except:
+            print("Something went wrong, try again!")        
